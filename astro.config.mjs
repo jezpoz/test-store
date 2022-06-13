d797c6e4c32923e1b23847b925556295bfb8033e
jezpoz <jesfor94@gmail.com>
2022-06-13 12:07:29 +0200

    SSR in vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwind from "@astrojs/tailwind";
 import turbolinks from "@astrojs/turbolinks";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   // No integrations are needed for AlpineJS support, just use Astro components!
+  adapter: vercel(),
   integrations: [tailwind(), turbolinks(), partytown(), sitemap()]
 });
